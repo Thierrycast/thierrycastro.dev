524cfd4457b429ad74ae67691c551e5b54beb1f3
Thierry Castro <thierrycastro555@gmail.com>
2025-04-21 23:16:15 -0300

    refactor: migra estrutura para app/[locale] com suporte a múltiplos idiomas

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,33 @@
+import { getLocale } from 'next-intl/server';
+import { buildHomeLabels } from '@/i18n/buildHomeLabels';
+
+import Header from '../components/Header';
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
+import ProjectsSection from '../components/ProjectsSection';
+import SkillsSection from '../components/SkillsSection';
+import TimelineSection from '../components/TimelineSection';
+
+export default async function Home() {
+  const locale = await getLocale();
+  const labels = await buildHomeLabels(locale);
+
+  return (
+    <main className="flex flex-col items-center">
+      <div
+        className="absolute top-0 w-full h-[750px] bg-cover bg-center -z-10"
+        style={{ backgroundImage: "url('/background_galaxy.svg')" }}
+      />
+      <Header labels={labels.header} />
+      <HeroSection labels={labels.hero} />
+      <AboutSection labels={labels.about} />
+      <ProjectsSection labels={labels.projects} />
+      < TimelineSection labels={labels.timeline} />
+      <SkillsSection labels={labels.skills} />
+      <ContactSection labels={labels.contact} />
+      <Footer labels={labels.footer} />
+    </main>
+  );
+}
