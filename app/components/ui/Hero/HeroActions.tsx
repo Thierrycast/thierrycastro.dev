29cf1b62064a76fbeb556b68660d1e4544be7392
Thierry Castro <thierrycastro555@gmail.com>
2025-05-03 16:20:10 -0300

    style: ajustes finos nos botoes de action

--- app/components/ui/Hero/HeroActions.tsx
+++ app/components/ui/Hero/HeroActions.tsx
@@ -5,7 +5,7 @@ import LanguageSwitcher from '../../LanguageSwitcher';
 
 export default function HeroActions() {
   return (
-    <div className="absolute flex gap-4 top-24 right-4">
+    <div className="absolute flex items-center gap-4 top-24 right-4">
       <ThemeSwitcher className="text-white" />
       <LanguageSwitcher />
     </div>
